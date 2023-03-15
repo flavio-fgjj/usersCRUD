f847523873bcdc178bf5d8bfefbefe042ea7adfd
Flavio Alvarenga <flavio.fgjj@live.com>
2023-03-15 11:38:44 -0300

    dependencies lib + icons and splash screen for android

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@routes': './src/routes',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
 };
